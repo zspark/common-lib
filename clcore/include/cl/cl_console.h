292f8ba139dcb2d347e9d496c5f2209bef131839
Jerry-Chaos <z_Spark@163.com>
2017-12-30 02:05:52 +0800

    rename

--- clcore/include/cl/cl_console.h
+++ clcore/include/cl/cl_console.h
@@ -6,102 +6,107 @@
 
 CL_NAMESPACE_START
 
-namespace ConForeColor{
-enum color{
-  BLACK=0,
-  DARKBLUE=FOREGROUND_BLUE,
-  DARKGREEN=FOREGROUND_GREEN,
-  DARKCYAN=FOREGROUND_GREEN|FOREGROUND_BLUE,
-  DARKRED=FOREGROUND_RED,
-  DARKMAGENTA=FOREGROUND_RED|FOREGROUND_BLUE,
-  DARKYELLOW=FOREGROUND_RED|FOREGROUND_GREEN,
-  DARKGRAY=FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE,
-  GRAY=FOREGROUND_INTENSITY,
-  BLUE=FOREGROUND_INTENSITY|FOREGROUND_BLUE,
-  GREEN=FOREGROUND_INTENSITY|FOREGROUND_GREEN,
-  CYAN=FOREGROUND_INTENSITY|FOREGROUND_GREEN|FOREGROUND_BLUE,
-  RED=FOREGROUND_INTENSITY|FOREGROUND_RED,
-  MAGENTA=FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_BLUE,
-  YELLOW=FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_GREEN,
-  WHITE=FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE,
-};
+namespace clConForeColor {
+	enum color {
+		BLACK = 0,
+		DARKBLUE = FOREGROUND_BLUE,
+		DARKGREEN = FOREGROUND_GREEN,
+		DARKCYAN = FOREGROUND_GREEN | FOREGROUND_BLUE,
+		DARKRED = FOREGROUND_RED,
+		DARKMAGENTA = FOREGROUND_RED | FOREGROUND_BLUE,
+		DARKYELLOW = FOREGROUND_RED | FOREGROUND_GREEN,
+		DARKGRAY = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE,
+		GRAY = FOREGROUND_INTENSITY,
+		BLUE = FOREGROUND_INTENSITY | FOREGROUND_BLUE,
+		GREEN = FOREGROUND_INTENSITY | FOREGROUND_GREEN,
+		CYAN = FOREGROUND_INTENSITY | FOREGROUND_GREEN | FOREGROUND_BLUE,
+		RED = FOREGROUND_INTENSITY | FOREGROUND_RED,
+		MAGENTA = FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_BLUE,
+		YELLOW = FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN,
+		WHITE = FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE,
+	};
 }
 
-namespace ConBackColor{
-enum color{
-  BLACK=0,
-  DARKBLUE=BACKGROUND_BLUE,
-  DARKGREEN=BACKGROUND_GREEN,
-  DARKCYAN=BACKGROUND_GREEN|BACKGROUND_BLUE,
-  DARKRED=BACKGROUND_RED,
-  DARKMAGENTA=BACKGROUND_RED|BACKGROUND_BLUE,
-  DARKYELLOW=BACKGROUND_RED|BACKGROUND_GREEN,
-  DARKGRAY=BACKGROUND_RED|BACKGROUND_GREEN|BACKGROUND_BLUE,
-  GRAY=BACKGROUND_INTENSITY,
-  BLUE=BACKGROUND_INTENSITY|BACKGROUND_BLUE,
-  GREEN=BACKGROUND_INTENSITY|BACKGROUND_GREEN,
-  CYAN=BACKGROUND_INTENSITY|BACKGROUND_GREEN|BACKGROUND_BLUE,
-  RED=BACKGROUND_INTENSITY|BACKGROUND_RED,
-  MAGENTA=BACKGROUND_INTENSITY|BACKGROUND_RED|BACKGROUND_BLUE,
-  YELLOW=BACKGROUND_INTENSITY|BACKGROUND_RED|BACKGROUND_GREEN,
-  WHITE=BACKGROUND_INTENSITY|BACKGROUND_RED|BACKGROUND_GREEN|BACKGROUND_BLUE,
-};
+namespace clConBackColor {
+	enum color {
+		BLACK = 0,
+		DARKBLUE = BACKGROUND_BLUE,
+		DARKGREEN = BACKGROUND_GREEN,
+		DARKCYAN = BACKGROUND_GREEN | BACKGROUND_BLUE,
+		DARKRED = BACKGROUND_RED,
+		DARKMAGENTA = BACKGROUND_RED | BACKGROUND_BLUE,
+		DARKYELLOW = BACKGROUND_RED | BACKGROUND_GREEN,
+		DARKGRAY = BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE,
+		GRAY = BACKGROUND_INTENSITY,
+		BLUE = BACKGROUND_INTENSITY | BACKGROUND_BLUE,
+		GREEN = BACKGROUND_INTENSITY | BACKGROUND_GREEN,
+		CYAN = BACKGROUND_INTENSITY | BACKGROUND_GREEN | BACKGROUND_BLUE,
+		RED = BACKGROUND_INTENSITY | BACKGROUND_RED,
+		MAGENTA = BACKGROUND_INTENSITY | BACKGROUND_RED | BACKGROUND_BLUE,
+		YELLOW = BACKGROUND_INTENSITY | BACKGROUND_RED | BACKGROUND_GREEN,
+		WHITE = BACKGROUND_INTENSITY | BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE,
+	};
 }
 
-typedef void(*fn_print)(DWORD currentC,clbool newline,const clstr& s,clstr mark);
 
-class CL_API clConsole{
+namespace clCon {
 
-public:
-  static clConsole* GetSingalton();
-  static void Release();
+	typedef void(*fn_print)(DWORD currentC, clbool newline, const clstr& s, clstr mark);
 
-public:
-  void Info(const clstr& s,clbool newLine=true);
-  void Warning(const clstr& s,clbool newLine=true);
-  void Error(const clstr& s,clbool newLine=true);
+	class CL_API clConsole {
 
-  void Text(const clstr& s,clbool newLine=true);
-  void HighLightText(const clstr& s,std::vector<cluint>& pos,clint posColor
-    ,clbool newLine=true);
-  void NewLine();
+	public:
+		static clConsole* GetSingalton();
+		static void Release();
 
-  /**
-   *  用c填充当前行剩余的位置，然后另起一行；
-   */
-  void FillRestWith(clchar c);
-  void UpdateLastNChars(const clstr& s,cluint n);
-  void Input(clchar* c);
+	public:
+		void Info(const clstr& s, clbool newLine = true);
+		void Warning(const clstr& s, clbool newLine = true);
+		void Error(const clstr& s, clbool newLine = true);
 
-  void SetWorkingFlag(clbool b);
-  void SetMark(clstr c){ m_mark=c; }
-  void SetDefaultColor(ConForeColor::color defaultColor,ConBackColor::color backgroundColor);
-  void SetInfoColor(ConForeColor::color defaultColor,ConBackColor::color backgroundColor);
-  void SetWarningColor(ConForeColor::color defaultColor,ConBackColor::color backgroundColor);
-  void SetErrorColor(ConForeColor::color defaultColor,ConBackColor::color backgroundColor);
-  void SetInputColor(ConForeColor::color defaultColor,ConBackColor::color backgroundColor);
+		void Text(const clstr& s, clbool newLine = true);
+		void HighLightText(const clstr& s, std::vector<cluint>& pos, clint posColor
+			, clbool newLine = true);
+		void NewLine();
 
-  void SetTitle(clstr title);
+		/**
+		 *  用c填充当前行剩余的位置，然后另起一行；
+		 */
+		void FillRestWith(clchar c);
+		void UpdateLastNChars(const clstr& s, cluint n);
+		void Input(clchar* c);
 
-private:
-  clConsole();
-  clConsole(const clConsole&)=delete;
-  clConsole& operator=(const clConsole&)=delete;
-  ~clConsole();
+		void SetWorkingFlag(clbool b);
+		void SetMark(clstr c) { m_mark = c; }
+		void SetDefaultColor(clConForeColor::color defaultColor, clConBackColor::color backgroundColor);
+		void SetInfoColor(clConForeColor::color defaultColor, clConBackColor::color backgroundColor);
+		void SetWarningColor(clConForeColor::color defaultColor, clConBackColor::color backgroundColor);
+		void SetErrorColor(clConForeColor::color defaultColor, clConBackColor::color backgroundColor);
+		void SetInputColor(clConForeColor::color defaultColor, clConBackColor::color backgroundColor);
 
-private:
-  DWORD m_conDefaultColor;
-  DWORD m_conInfoColor;
-  DWORD m_conWarningColor;
-  DWORD m_conErrorColor;
-  DWORD m_conInputColor;
-  DWORD m_originColor;
-  cluint m_uFlag=0u;
+		void SetTitle(clstr title);
 
-  clstr m_mark="";
+	private:
+		clConsole();
+		clConsole(const clConsole&) = delete;
+		clConsole& operator=(const clConsole&) = delete;
+		~clConsole();
 
-  fn_print m_tracer=nullptr;
+	private:
+		DWORD m_conDefaultColor;
+		DWORD m_conInfoColor;
+		DWORD m_conWarningColor;
+		DWORD m_conErrorColor;
+		DWORD m_conInputColor;
+		DWORD m_originColor;
+		cluint m_uFlag = 0u;
 
-};
+		clstr m_mark = "";
+
+		fn_print m_tracer = nullptr;
+
+	};
+
+}
 
 CL_NAMESPACE_END
--- clcore/src/cl_console.cpp
+++ clcore/src/cl_console.cpp
@@ -3,184 +3,190 @@
 
 using namespace std;
 
+
 #define WORKING 1<<2
 
-HANDLE s_handler=NULL;
+HANDLE s_handler = NULL;
 
-void static sConsPrint_none(DWORD currentC,clbool newline,const clstr& s,clstr mark){}
+void static sConsPrint_none(DWORD currentC, clbool newline, const clstr& s, clstr mark) {}
 
-void static sConsPrint(DWORD currentC,clbool newline,const clstr& s,clstr mark){
-  SetConsoleTextAttribute(s_handler,currentC);
-  cout<<mark;
-  cout<<s;
-  if(newline)cout<<endl;
+void static sConsPrint(DWORD currentC, clbool newline, const clstr& s, clstr mark) {
+	SetConsoleTextAttribute(s_handler, currentC);
+	cout << mark;
+	cout << s;
+	if (newline)cout << endl;
 }
 
 /**
  * only works at current line
  */
-void sCursorBackNChars(cluint n){
-  CONSOLE_SCREEN_BUFFER_INFO bufInfo;
-  GetConsoleScreenBufferInfo(s_handler,&bufInfo);
-  COORD coord=bufInfo.dwCursorPosition;
-  int tmp=coord.X-n;
-  if(tmp<0)tmp=0;
-  coord.X=tmp;
-  SetConsoleCursorPosition(s_handler,coord);
+void sCursorBackNChars(cluint n) {
+	CONSOLE_SCREEN_BUFFER_INFO bufInfo;
+	GetConsoleScreenBufferInfo(s_handler, &bufInfo);
+	COORD coord = bufInfo.dwCursorPosition;
+	int tmp = coord.X - n;
+	if (tmp < 0)tmp = 0;
+	coord.X = tmp;
+	SetConsoleCursorPosition(s_handler, coord);
 }
 
-void sCleanCurrentLineBeheadCursor(){
-  CONSOLE_SCREEN_BUFFER_INFO bufInfo;
-  GetConsoleScreenBufferInfo(s_handler,&bufInfo);
-  COORD coord=bufInfo.dwCursorPosition;
-  cluint n=bufInfo.dwSize.X-coord.X;
-  WriteConsole(s_handler,"",n,NULL,NULL);
-  SetConsoleCursorPosition(s_handler,coord);
+void sCleanCurrentLineBeheadCursor() {
+	CONSOLE_SCREEN_BUFFER_INFO bufInfo;
+	GetConsoleScreenBufferInfo(s_handler, &bufInfo);
+	COORD coord = bufInfo.dwCursorPosition;
+	cluint n = bufInfo.dwSize.X - coord.X;
+	WriteConsole(s_handler, "", n, NULL, NULL);
+	SetConsoleCursorPosition(s_handler, coord);
 }
 
 
 
 CL_NAMESPACE_START
 
-static clConsole* s_ins=nullptr;
+namespace clCon {
 
-clConsole * clConsole::GetSingalton(){
-  if(s_ins)return s_ins;
-  else{
-    s_ins=new clConsole();
-    return s_ins;
-  }
-}
+	static clConsole* s_ins = nullptr;
 
-void clConsole::Release(){
-  if(s_ins)delete s_ins;
-  s_handler=NULL;
-  system("pause");
-}
+	clConsole * clConsole::GetSingalton() {
+		if (s_ins)return s_ins;
+		else {
+			s_ins = new clConsole();
+			return s_ins;
+		}
+	}
 
-void clConsole::Info(const clstr & s,clbool newLine){
-  m_tracer(m_conInfoColor,newLine,s,m_mark);
-}
+	void clConsole::Release() {
+		if (s_ins)delete s_ins;
+		s_handler = NULL;
+		system("pause");
+	}
 
-void clConsole::Warning(const clstr & s,clbool newLine){
-  m_tracer(m_conWarningColor,newLine,s,m_mark);
-}
+	void clConsole::Info(const clstr & s, clbool newLine) {
+		m_tracer(m_conInfoColor, newLine, s, m_mark);
+	}
 
-void clConsole::Error(const clstr & s,clbool newLine){
-  m_tracer(m_conErrorColor,newLine,s,m_mark);
-}
+	void clConsole::Warning(const clstr & s, clbool newLine) {
+		m_tracer(m_conWarningColor, newLine, s, m_mark);
+	}
 
-void clConsole::Text(const clstr& s,clbool newLine){
-  m_tracer(m_conDefaultColor,newLine,s,m_mark);
-}
+	void clConsole::Error(const clstr & s, clbool newLine) {
+		m_tracer(m_conErrorColor, newLine, s, m_mark);
+	}
 
-void clConsole::Input(clchar* c){
-  if(m_uFlag&WORKING)return;
-  SetConsoleTextAttribute(s_handler,m_conInputColor);
-  cin.get(c,256);
-  rewind(stdin);
-}
+	void clConsole::Text(const clstr& s, clbool newLine) {
+		m_tracer(m_conDefaultColor, newLine, s, m_mark);
+	}
 
-void clConsole::HighLightText(
-  const clstr& s,vector<cluint>& pos,clint posColor,clbool newLine)
-{
-  if(m_uFlag&WORKING)return;
-  SetConsoleTextAttribute(s_handler,m_conDefaultColor);
-  clint index=0;
-  const clint N=pos.size();
-  for(clint i=0;i<N;i+=2){
-    clint posAti=pos[i];
-    clint len=pos[i+1];
-    cout<<s.substr(index,posAti-index);
-    SetConsoleTextAttribute(s_handler,posColor);
-    cout<<s.substr(posAti,len);
-    index=posAti+len;
-  }
-  if(index<s.length()){
-    SetConsoleTextAttribute(s_handler,m_conDefaultColor);
-    cout<<s.substr(index,s.length()-index);
-  }
-  if(newLine)cout<<endl;
-}
+	void clConsole::Input(clchar* c) {
+		if (m_uFlag&WORKING)return;
+		SetConsoleTextAttribute(s_handler, m_conInputColor);
+		cin.get(c, 256);
+		rewind(stdin);
+	}
 
-void clConsole::NewLine(){ cout<<endl; }
+	void clConsole::HighLightText(
+		const clstr& s, vector<cluint>& pos, clint posColor, clbool newLine)
+	{
+		if (m_uFlag&WORKING)return;
+		SetConsoleTextAttribute(s_handler, m_conDefaultColor);
+		clint index = 0;
+		const clint N = pos.size();
+		for (clint i = 0; i < N; i += 2) {
+			clint posAti = pos[i];
+			clint len = pos[i + 1];
+			cout << s.substr(index, posAti - index);
+			SetConsoleTextAttribute(s_handler, posColor);
+			cout << s.substr(posAti, len);
+			index = posAti + len;
+		}
+		if (index < s.length()) {
+			SetConsoleTextAttribute(s_handler, m_conDefaultColor);
+			cout << s.substr(index, s.length() - index);
+		}
+		if (newLine)cout << endl;
+	}
 
-void clConsole::FillRestWith(clchar c){
-  CONSOLE_SCREEN_BUFFER_INFO bufInfo;
-  GetConsoleScreenBufferInfo(s_handler,&bufInfo);
-  size_t N=bufInfo.dwSize.X-bufInfo.dwCursorPosition.X;
-  clstr s(N,c);
-  m_tracer(-1,false,s,"");
-}
+	void clConsole::NewLine() { cout << endl; }
 
-void clConsole::UpdateLastNChars(const clstr & s,cluint n){
-  sCursorBackNChars(n);
-  WriteConsole(s_handler,s.c_str(),s.length(),NULL,NULL);
-}
+	void clConsole::FillRestWith(clchar c) {
+		CONSOLE_SCREEN_BUFFER_INFO bufInfo;
+		GetConsoleScreenBufferInfo(s_handler, &bufInfo);
+		size_t N = bufInfo.dwSize.X - bufInfo.dwCursorPosition.X;
+		clstr s(N, c);
+		m_tracer(-1, false, s, "");
+	}
 
-void clConsole::SetDefaultColor(ConForeColor::color defaultColor,ConBackColor::color backgroundColor){
-  m_conDefaultColor=defaultColor|backgroundColor;
-}
+	void clConsole::UpdateLastNChars(const clstr & s, cluint n) {
+		sCursorBackNChars(n);
+		WriteConsole(s_handler, s.c_str(), s.length(), NULL, NULL);
+	}
 
-void clConsole::SetInfoColor(ConForeColor::color defaultColor,ConBackColor::color backgroundColor){
-  m_conInfoColor=defaultColor|backgroundColor;
-}
+	void clConsole::SetDefaultColor(clConForeColor::color defaultColor, clConBackColor::color backgroundColor) {
+		m_conDefaultColor = defaultColor | backgroundColor;
+	}
 
-void clConsole::SetWarningColor(ConForeColor::color defaultColor,ConBackColor::color backgroundColor){
-  m_conWarningColor=defaultColor|backgroundColor;
-}
+	void clConsole::SetInfoColor(clConForeColor::color defaultColor, clConBackColor::color backgroundColor) {
+		m_conInfoColor = defaultColor | backgroundColor;
+	}
 
-void clConsole::SetErrorColor(ConForeColor::color defaultColor,ConBackColor::color backgroundColor){
-  m_conErrorColor=defaultColor|backgroundColor;
-}
+	void clConsole::SetWarningColor(clConForeColor::color defaultColor, clConBackColor::color backgroundColor) {
+		m_conWarningColor = defaultColor | backgroundColor;
+	}
 
-void clConsole::SetInputColor(ConForeColor::color defaultColor,ConBackColor::color backgroundColor){
-  m_conInputColor=defaultColor|backgroundColor;
-}
+	void clConsole::SetErrorColor(clConForeColor::color defaultColor, clConBackColor::color backgroundColor) {
+		m_conErrorColor = defaultColor | backgroundColor;
+	}
 
-void clConsole::SetTitle(clstr title){
-  ::SetConsoleTitle(title.c_str());
-}
+	void clConsole::SetInputColor(clConForeColor::color defaultColor, clConBackColor::color backgroundColor) {
+		m_conInputColor = defaultColor | backgroundColor;
+	}
 
-void clConsole::SetWorkingFlag(clbool b){
-  if(b){
-    m_uFlag|=WORKING;
-    m_tracer=sConsPrint_none;
-  } else{
-    m_uFlag&=~WORKING;
-    m_tracer=sConsPrint;
-  }
-}
+	void clConsole::SetTitle(clstr title) {
+		::SetConsoleTitle(title.c_str());
+	}
 
-clConsole::clConsole(){
-  s_handler=GetStdHandle(STD_OUTPUT_HANDLE);
-  CONSOLE_SCREEN_BUFFER_INFO bufInfo;
-  GetConsoleScreenBufferInfo(s_handler,&bufInfo);
-  m_originColor=bufInfo.wAttributes;
-  
-  DWORD backColor=m_originColor&0x00F0;
-  m_conDefaultColor=ConForeColor::DARKGRAY|backColor;
-  m_conInfoColor=ConForeColor::GREEN|backColor;
-  m_conWarningColor=ConForeColor::YELLOW|backColor;
-  m_conErrorColor=ConForeColor::RED|backColor;
-  m_conInputColor=ConForeColor::BLUE|ConBackColor::WHITE;
+	void clConsole::SetWorkingFlag(clbool b) {
+		if (b) {
+			m_uFlag |= WORKING;
+			m_tracer = sConsPrint_none;
+		}
+		else {
+			m_uFlag &= ~WORKING;
+			m_tracer = sConsPrint;
+		}
+	}
 
-/*
-  COORD coord;coord.X=80; coord.Y=10;
-  SetConsoleScreenBufferSize(s_handler,coord);
-  coord.X=0; coord.Y=0;
-  DWORD att=ConForeColor::WHITE|ConBackColor::DARKBLUE;
-  const DWORD N=bufInfo.dwSize.X*bufInfo.dwSize.Y;
-  DWORD n;
-  FillConsoleOutputAttribute(s_handler,att,N,coord,&n);
-  */
+	clConsole::clConsole() {
+		s_handler = GetStdHandle(STD_OUTPUT_HANDLE);
+		CONSOLE_SCREEN_BUFFER_INFO bufInfo;
+		GetConsoleScreenBufferInfo(s_handler, &bufInfo);
+		m_originColor = bufInfo.wAttributes;
 
-  m_tracer=sConsPrint;
-}
+		DWORD backColor = m_originColor & 0x00F0;
+		m_conDefaultColor = clConForeColor::DARKGRAY | backColor;
+		m_conInfoColor = clConForeColor::GREEN | backColor;
+		m_conWarningColor = clConForeColor::YELLOW | backColor;
+		m_conErrorColor = clConForeColor::RED | backColor;
+		m_conInputColor = clConForeColor::BLUE | clConBackColor::WHITE;
 
-clConsole::~clConsole(){
-  m_tracer=nullptr;
-  SetConsoleTextAttribute(s_handler,m_originColor);
+		/*
+			COORD coord;coord.X=80; coord.Y=10;
+			SetConsoleScreenBufferSize(s_handler,coord);
+			coord.X=0; coord.Y=0;
+			DWORD att=clConForeColor::WHITE|clConBackColor::DARKBLUE;
+			const DWORD N=bufInfo.dwSize.X*bufInfo.dwSize.Y;
+			DWORD n;
+			FillConsoleOutputAttribute(s_handler,att,N,coord,&n);
+			*/
+
+		m_tracer = sConsPrint;
+	}
+
+	clConsole::~clConsole() {
+		m_tracer = nullptr;
+		SetConsoleTextAttribute(s_handler, m_originColor);
+	}
+
 }
 
 CL_NAMESPACE_END
